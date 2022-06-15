76f469d1bbbcb24cbe912054dcb2fde6f69ddf21
root <root@HomeLPT-001.localdomain>
2022-06-15 21:59:31 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+
+const main=document.getElementById('main');
+main.remove();
+
+const newHeader=document.createElement('h1')
+newHeader.id="victory"
+newHeader.textContent="YOUR-NAME is the champion"
+document.body.append(newHeader);
